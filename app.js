85ecd41d59a8083c01d27ed71781ff42b3de4c98
Manuel Antonio Rosete <rosetema@globalhitss.com>
2021-01-20 18:25:56 -0600

    Chat

--- app.js
+++ app.js
@@ -3,22 +3,33 @@ const app = require('express')();
 const cors = require('cors');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const mongoose = require("mongoose");
+
+const sockets = [];
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors());
 
 io.on('connection', async (socket) => {
+    const s = {
+        id: socket.id,
+        name: socket.handshake.query.name,
+    };
 
-    console.log('Connection ID:', socket.id);
+    socket.broadcast.emit('userConnected', s.name);
+
+    sockets.push(s);
+    console.log('Connection:', s);
 
     socket.on(`message`, async (message, cb) => {
-
+        console.log({ from: s.name, message});
+        io.emit('newMessage', JSON.stringify({ from: s.name, message}));
     });
 
-    socket.on('disconnect', async function() {
 
+    socket.on('disconnect', async function() {
+        const index = sockets.findIndex(s => s.id === socket.id);
+        sockets.splice(index, 1);
     });
 
 });
